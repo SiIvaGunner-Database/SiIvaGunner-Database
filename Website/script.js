4da6f2c9e4121fb2b8c89a6608450a256212266c
AAAAAAidan <a.k.zamboni@gmail.com>
2020-06-02 15:54:35 -0700

    Minor syntax fix.

--- Website/script.js
+++ Website/script.js
@@ -2,7 +2,7 @@ var appUrl = "https://script.google.com/macros/s/AKfycbzm47208rTvNpu2pa-vhqdT53k
 
 function generateTemplate()
 {
-  var id = document.getElementById("textBox").value
+  var id = document.getElementById("textBox").value;
   id = id.replace("{\"\":\"", "").replace("\"}", "").replace("&feature=youtu.be", "").replace(/&.*/, "").replace(/h.*=/, "");
   var url = appUrl + "?type=generator&id=" + id;
   var xhttp;
@@ -20,26 +20,25 @@ function generateTemplate()
 
       document.getElementById("template").rows = lineCount;
       document.getElementById("template").innerHTML = this.responseText;
+
       if (this.responseText.indexOf("\n") != -1)
         document.getElementById("thumbnail").innerHTML = "<img src=\"https://img.youtube.com/vi/" + id + "/maxresdefault.jpg\" alt=\"Thumbnail\">";
     }
-  };
-
+  }
   xhttp.open("GET", url, false);
   xhttp.send();
 }
 
 function fileReport()
 {
-  var page = document.getElementById("page").value
-  var id = document.getElementById("id").value
-  var desc = document.getElementById("desc").value
+  var page = document.getElementById("page").value;
+  var id = document.getElementById("id").value;
+  var desc = document.getElementById("desc").value;
 
   if (id == "")
     document.getElementById("response").innerHTML = "Please enter the URL or ID that caused the problem.";
   else
   {
-    // I need to add encoding to the link.
     var url = appUrl + "?type=report&page=" + page  + "&id=" + id + "&desc=" + desc;
     var xhttp;
 
