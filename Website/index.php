95fe1c397ffe8544f0e5fabfb32dc8354b749af2
AAAAAAidan <a.k.zamboni@gmail.com>
2020-06-10 16:01:32 -0700

    Remove rip.php, add search and event functions, and add template formatting option.

--- Website/index.php
+++ Website/index.php
@@ -10,8 +10,24 @@
     <?php include("nav.html"); ?>
     <main>
       <div id="searchContainer">
-        <input type="text" id="inputText" class="textBox" placeholder="Search by video title or ID"/>
+        <input type="text" id="inputText" class="searchBox" placeholder="Search by title, URL, or ID"/>
         <button id="submitBtn" class="searchBtn" onclick="search()"></button>
+      </div>
+      <br/>
+      <div id="video">
+        <iframe id="videoEmbed" width="560px" height="315px" src="https://www.youtube.com/embed/NzoneDE0A2o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div id="videoInfo">
+        Video status: public<br/>
+        Wiki status: documented
+      </div>
+      <div id="videoDesc">
+        Music: The Inn<br/>
+        Composer: Yuka Tsujiyoko<br/>
+        Playlist: https://www.youtube.com/playlist?list=PLL0CQjrcN8D1CYB8alWM5bax6yGk83dZ8<br/>
+        Platform: Game Boy Advance<br/><br/>
+
+        Go-Go Gadget Channel Description!
       </div>
     </main>
     <?php include("footer.html"); ?>
--- Website/script.js
+++ Website/script.js
@@ -1,16 +1,88 @@
 var appUrl = "https://script.google.com/macros/s/AKfycbzm47208rTvNpu2pa-vhqdT53k9_F1l0j47nqlJOw/exec";
-var reay = false;
+var lastSelection = "single";
+document.onkeydown = checkKey;
+if (document.location.pathname.match("generator.php"))
+{
+  document.onclick = checkSelection;
+  window.onload = load;
+}
 
-document.onkeydown = logKey;
+function load()
+{
+  var str = "{{Rip" +
+            "\n|image= Fire Emblem.jpg" +
+            "\n" +
+            "\n|link= NzoneDE0A2o" +
+            "\n|playlist= Fire Emblem" +
+            "\n|playlist id= PLL0CQjrcN8D1CYB8alWM5bax6yGk83dZ8" +
+            "\n|upload= April 7, 2018" +
+            "\n|length= 3:25" +
+            "\n|author= " +
+            "\n" +
+            "\n|album= " +
+            "\n|track= " +
+            "\n" +
+            "\n|music= The Inn" +
+            "\n|composer= Yuka Tsujiyoko" +
+            "\n|platform= Game Boy Advance" +
+            "\n|catchphrase= Go-Go Gadget Channel Description!" +
+            "\n}}" +
+            "\n\"'''The Inn - Fire Emblem'''\" is a high quality rip of \"The Inn\" from ''Fire Emblem''." +
+             "\n== Jokes ==";
 
-function logKey(e) {
+  document.getElementById("template").rows = 21;
+  document.getElementById("template").innerHTML = str;
+}
+
+function checkKey(e)
+{
   if (e.code == "Enter" && document.getElementById("inputText").value != "")
     document.getElementById("submitBtn").click();
 }
 
+function checkSelection()
+{
+  var selection = document.getElementById("format").value;
+
+  if (selection != lastSelection)
+  {
+    var template = document.getElementById("template").value;
+    template = template.toString().replace("== Jokes ==", "JOKEHEADER");
+    lastSelection = selection;
+
+    if (selection == "none")
+      template = template.replace(/\t\t= |\t= | = |= /g, "=");
+    else if (selection == "single")
+      template = template.replace(/\t\t= |\t= | = |=/g, "= ");
+    else if (selection == "double")
+      template = template.replace(/\t\t= |\t= |= |=/g, " = ");
+    else if (selection == "tab")
+    {
+      template = template.replace(/ = |= |=/g, "\t\t= ");
+      template = template.replace("playlist\t\t", "playlist\t");
+      template = template.replace("playlist id\t\t", "playlist id\t");
+      template = template.replace("composer\t\t", "composer\t");
+      template = template.replace("composer label\t\t", "composer label\t");
+      template = template.replace("platform\t\t", "platform\t");
+      template = template.replace("platform label\t\t", "platform label\t");
+      template = template.replace("catchphrase\t\t", "catchphrase\t");
+    }
+
+    template = template.replace("JOKEHEADER", "== Jokes ==");
+    document.getElementById("template").innerHTML = template;
+  }
+}
+
 function search()
 {
+  var input = document.getElementById("inputText").value;
 
+  if (input.length != 11 && input.indexOf(" ") == -1)
+    input = input.replace("{\"\":\"", "").replace("\"}", "").replace("&feature=youtu.be", "").replace(/&.*/, "").replace(/h.*=/, "");
+
+  var url = appUrl + "?type=search&input=" + input;
+
+  getUrlResponse(url, "search");
 }
 
 function copyTemplate()
@@ -25,9 +97,10 @@ function copyTemplate()
 
 function generateTemplate()
 {
+  var format = document.getElementById("format").value;
   var id = document.getElementById("inputText").value;
   id = id.replace("{\"\":\"", "").replace("\"}", "").replace("&feature=youtu.be", "").replace(/&.*/, "").replace(/h.*=/, "");
-  var url = appUrl + "?type=template&id=" + id;
+  var url = appUrl + "?type=template&format=" + format + "&id=" + id;
 
   getUrlResponse(url, "template");
 }
@@ -39,7 +112,7 @@ function report()
   var desc = document.getElementById("desc").value;
 
   if (id == "")
-    document.getElementById("response").innerHTML = "Please enter the URL or ID that caused the problem.";
+    document.getElementById("response").innerHTML = "Please enter the title, URL, or ID that caused the problem.";
   else
   {
     var url = appUrl + "?type=report&page=" + page  + "&id=" + id + "&desc=" + desc;
@@ -62,7 +135,18 @@ function getUrlResponse(url, type)
     {
       var response = this.responseText;
 
-      if (type == "template")
+      if (type == "search")
+      {
+        if (response.indexOf("SEPARATOR") != -1)
+        {
+          response = response.replace(/\n/g, "<br/>").split("SEPARATOR");
+          document.getElementById("videoEmbed").src = "https://www.youtube.com/embed/" + response[0];
+          document.getElementById("videoInfo").innerHTML = response[1];
+          document.getElementById("videoDesc").innerHTML = response[2];
+        } else
+          alert(response);
+
+      } else if (type == "template")
       {
         var id = url.split("id=").pop();
         var lineCount = response.split(/\n/).length + 1;
@@ -76,5 +160,5 @@ function getUrlResponse(url, type)
       } else if (type == "report")
         document.getElementById("response").innerHTML = response;
     }
-  };
+  }
 }
