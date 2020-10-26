033de47ef9bcee110f072de8ceefceddcc824da9
AAAAAAidan <a.k.zamboni@gmail.com>
2020-10-25 20:34:21 -0700

    Add .htaccess and error 404 page

---
+++ website-files/404.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+  <?php include("head.php"); ?>
+  <body>
+    <?php include("nav.html"); ?>
+    <main>
+        <h3>Error 404</h3>
+        Page not found.
+    </main>
+    <?php include("footer.html"); ?>
+  </body>
+</html>
