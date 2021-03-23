377bb831b224332ada03d95db5f1101891315018
Aidan <a.k.zamboni@gmail.com>
2021-03-22 17:10:12 -0700

    Implement search saving

--- siivagunnerdb/channels/views.py
+++ siivagunnerdb/channels/views.py
@@ -1,25 +1,58 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models.functions import Lower
 from django.utils.text import slugify
 from rips.models import Rip
+from django.urls import reverse
+from urllib.parse import urlencode
 from .models import Channel
 from . import forms
 
 def channelList(request):
+    # If the search is being submitted
     if request.method == 'POST':
-        searchTerms = request.POST['searchTerms']
-        sort = request.POST['sort']
-        sortType = request.POST['sortType']
-        channelsByTitle = Channel.objects.filter(visible=True, name__icontains=searchTerms).order_by(sortType + sort)
-        channelsById = Channel.objects.filter(visible=True, channelId__icontains=searchTerms).order_by(sortType + sort)
-        channels = channelsByTitle | channelsById
-        return render(request, 'channels/channelList.html', { 'channels':channels })
-    channels = Channel.objects.filter(visible=True).order_by('-joinDate')
-    return render(request, 'channels/channelList.html', {'channels': channels })
+        parameters = []
+        if request.POST['searchTerms']:
+            queryString =  urlencode({'search': request.POST['searchTerms']})  # param=val
+            parameters.append(queryString)
+        if request.POST['sort'] != 'joinDate':
+            queryString =  urlencode({'sort': request.POST['sort']})  # param=val
+            parameters.append(queryString)
+        if request.POST['sortType'] != 'descending':
+            queryString =  urlencode({'order': request.POST['sortType']})  # param=val
+            parameters.append(queryString)
+        url = reverse('channels:list')  # /channels/
+        paramChar = '?'
+        for param in parameters:
+            url = url + paramChar + param  # /channels/?param=val&param=val
+            paramChar = '&'
+        return redirect(url)
+
+    # Else load the search parameters if they exist or query the defaults
+    else:
+        if request.GET.get('search'):
+            search = request.GET.get('search')
+        else:
+            search = None
+        if request.GET.get('sort'):
+            sort = request.GET.get('sort')
+        else:
+            sort = "joinDate"
+        if request.GET.get('order') and request.GET.get('order') == "ascending":
+            order = ""
+        else:
+            order = "-"
+        if search:
+            channelsByName = Channel.objects.filter(visible=True, name__icontains=search)
+            channelsById = Channel.objects.filter(visible=True, channelId__icontains=search)
+            channels = (channelsByName | channelsById).order_by(order + sort)
+        else:
+            channels = Channel.objects.filter(visible=True).order_by(order + sort)
+        return render(request, 'channels/channelList.html', {'channels': channels })
 
 def channelDetails(request, channelSlug):
     channel = Channel.objects.get(slug=channelSlug)
-    rips = Rip.objects.filter(visible=True, channel__slug=channelSlug).order_by('-uploadDate')[:100]
+    rips = Rip.objects.filter(visible=True, channel__slug=channelSlug).order_by('-uploadDate')[:10]
     return render(request, 'channels/channelDetails.html', {'channel': channel, 'rips':rips })
 
 def channelAdd(request):
--- siivagunnerdb/rips/views.py
+++ siivagunnerdb/rips/views.py
@@ -1,44 +1,90 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db.models.functions import Lower
 from django.utils.text import slugify
+from django.urls import reverse
+from urllib.parse import urlencode
 from .models import Rip
 from . import forms
+import re
 
 def ripList(request):
+    # If the search is being submitted
     if request.method == 'POST':
-        searchTerms = request.POST['searchTerms']
-        sort = request.POST['sort']
-        sortType = request.POST['sortType']
-        filter = request.POST['filter']
-        maxResults = int(request.POST['maxResults'])
-        if filter == 'Unfiltered':
-            if searchTerms:
-                ripsByTitle = Rip.objects.filter(visible=True, title__icontains=searchTerms).order_by(sortType + sort)[:maxResults]
-                ripsById = Rip.objects.filter(visible=True, videoId__icontains=searchTerms).order_by(sortType + sort)[:maxResults]
-                ripsByChannel = Rip.objects.filter(visible=True, channel__name__icontains=searchTerms).order_by(sortType + sort)[:maxResults]
-                rips = ripsByTitle | ripsById | ripsByChannel
-            else:
-                rips = Rip.objects.filter(visible=True).order_by(sortType + sort)[:maxResults]
-        elif filter == 'Undocumented' or filter == 'Documented':
-            if searchTerms:
-                ripsByTitle = Rip.objects.filter(visible=True, title__icontains=searchTerms, wikiStatus=filter).order_by(sortType + sort)[:maxResults]
-                ripsById = Rip.objects.filter(visible=True, videoId__icontains=searchTerms, wikiStatus=filter).order_by(sortType + sort)[:maxResults]
-                ripsByChannel = Rip.objects.filter(visible=True, channel__name__icontains=searchTerms, wikiStatus=filter).order_by(sortType + sort)[:maxResults]
-                rips = ripsByTitle | ripsById | ripsByChannel
-            else:
-                rips = Rip.objects.filter(visible=True, wikiStatus=filter).order_by(sortType + sort)[:maxResults]
+        parameters = []
+        if request.POST['searchTerms']:
+            queryString =  urlencode({'search': request.POST['searchTerms']})  # param=val
+            parameters.append(queryString)
+        if request.POST['sort'] != 'date':
+            queryString =  urlencode({'sort': request.POST['sort']})  # param=val
+            parameters.append(queryString)
+        if request.POST['sortType'] != 'descending':
+            queryString =  urlencode({'order': request.POST['sortType']})  # param=val
+            parameters.append(queryString)
+        if request.POST['filter'] != 'unfiltered':
+            queryString =  urlencode({'filter': request.POST['filter']})  # param=val
+            parameters.append(queryString)
+        url = reverse('rips:list')  # /rips/
+        paramChar = '?'
+        for param in parameters:
+            url = url + paramChar + param  # /rips/?param=val&param=val
+            paramChar = '&'
+        return redirect(url)
+
+    # Else load the search parameters if they exist or query the defaults
+    else:
+        if request.GET.get('search'):
+            search = request.GET.get('search')
         else:
-            if searchTerms:
-                ripsByTitle = Rip.objects.filter(visible=True, title__icontains=searchTerms, videoStatus=filter).order_by(sortType + sort)[:maxResults]
-                ripsById = Rip.objects.filter(visible=True, videoId__icontains=searchTerms, videoStatus=filter).order_by(sortType + sort)[:maxResults]
-                ripsByChannel = Rip.objects.filter(visible=True, channel__name__icontains=searchTerms, videoStatus=filter).order_by(sortType + sort)[:maxResults]
-                rips = ripsByTitle | ripsById | ripsByChannel
+            search = None
+        if request.GET.get('sort'):
+            sort = request.GET.get('sort')
+            if sort == "views":
+                sort = "viewCount"
+        else:
+            sort = "uploadDate"
+        if request.GET.get('order') and request.GET.get('order') == "ascending":
+            order = ""
+        else:
+            order = "-"
+        if request.GET.get('filter'):
+            filter = request.GET.get('filter')
+        else:
+            filter = None
+        try:
+            page = int(request.GET.get('page'))
+            if page < 1:
+                page = 1
+        except:
+            page = 1
+        if search:
+            ripsByTitle = Rip.objects.filter(visible=True, title__icontains=search)
+            ripsByChannel = Rip.objects.filter(visible=True, channel__name__icontains=search)
+            ripsById = Rip.objects.filter(visible=True, videoId__icontains=search)
+            rips = (ripsByTitle | ripsById | ripsByChannel).order_by(order + sort)
+        else:
+            rips = Rip.objects.filter(visible=True).order_by(order + sort)
+        if filter:
+            filter = filter.capitalize()
+            if filter == 'Undocumented' or filter == 'Documented':
+                rips = rips.filter(wikiStatus=filter)
             else:
-                rips = Rip.objects.filter(visible=True, videoStatus=filter).order_by(sortType + sort)[:maxResults]
-        return render(request, 'rips/ripList.html', { 'rips':rips })
-    rips = Rip.objects.filter(visible=True).order_by('-uploadDate')[:100]
-    return render(request, 'rips/ripList.html', { 'rips':rips })
+                rips = rips.filter(videoStatus=filter)
+        ripCount = rips.count()
+        rips = rips[page * 100 - 100:page * 100]
+        url = request.get_full_path()
+        url = re.sub('page=.*&', '', url, flags=re.DOTALL)
+        url = re.sub('&page=.*', '', url, flags=re.DOTALL)
+        try:
+            url.index('?')
+            pageStr = '&page='
+        except:
+            pageStr = '?page='
+        prevUrl = url + pageStr + str(page - 1)
+        nextUrl = url + pageStr + str(page + 1)
+        urls = [prevUrl, nextUrl]
+        return render(request, 'rips/ripList.html', { 'rips':rips, 'urls':urls, } )
 
 def ripDetails(request, ripSlug):
     rip = Rip.objects.get(slug=ripSlug)
