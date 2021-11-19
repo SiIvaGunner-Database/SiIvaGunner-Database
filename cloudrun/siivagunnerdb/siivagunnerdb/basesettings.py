6a4aa3daad7336fb2607310207c9a686773f5d97
AAAAAAidan <a.k.zamboni@gmail.com>
2021-11-18 17:25:41 -0700

    Add CSRF token, add logging

--- cloudrun/siivagunnerdb/siivagunnerdb/basesettings.py
+++ cloudrun/siivagunnerdb/siivagunnerdb/basesettings.py
@@ -81,6 +81,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'siivagunnerdb.wsgi.application'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
