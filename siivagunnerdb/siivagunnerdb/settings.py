02edb509aea790d56a7473dcdd98f27c4ceee208
AAAAAAidan <a.k.zamboni@gmail.com>
2022-03-12 20:05:08 -0700

    Ensure cloud settings are being used

--- siivagunnerdb/siivagunnerdb/settings.py
+++ siivagunnerdb/siivagunnerdb/settings.py
@@ -4,7 +4,7 @@ Default settings file loads settings for the appropriate environment.
 
 import os
 
-if os.environ.get("ENV_NAME"):
-    from .cloudsettings import *
-else:
-    from .localsettings import *
+# if os.environ.get("ENV_NAME"):
+from .cloudsettings import *
+# else:
+#     from .localsettings import *
