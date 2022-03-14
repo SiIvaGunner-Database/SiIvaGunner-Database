ed7bc25dc99213bb11a4fa5a4a94e9cdf2f22b48
AAAAAAidan <a.k.zamboni@gmail.com>
2022-03-13 19:02:12 -0700

    Add logging to settings.py

--- siivagunnerdb/siivagunnerdb/settings.py
+++ siivagunnerdb/siivagunnerdb/settings.py
@@ -4,7 +4,11 @@ Default settings file loads settings for the appropriate environment.
 
 import os
 
-# if os.environ.get("ENV_NAME"):
-from .cloudsettings import *
-# else:
-#     from .localsettings import *
+ENV_NAME = os.environ.get("ENV_NAME")
+
+if ENV_NAME:
+    print(ENV_NAME + " environment set. Using cloud settings.")
+    from .cloudsettings import *
+else:
+    print("No environment set. Using local settings.")
+    from .localsettings import *
