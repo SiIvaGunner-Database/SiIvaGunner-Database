2fcff5e3a17c03eec99f0166525e3f1665172339
AAAAAAidan <a.k.zamboni@gmail.com>
2022-04-13 20:06:06 -0700

    Fix middleware issue, update .gitignore, update notice messages

--- siivagunnerdb/siivagunnerdb/basesettings.py
+++ siivagunnerdb/siivagunnerdb/basesettings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
